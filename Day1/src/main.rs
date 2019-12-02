2442e244387f48fb4ae477cb3b786a4a105781a4
Derek Prince <ace.prince08@gmail.com>
2019-12-01 19:29:49 -0700

    Part 2 completed as well.

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -18,9 +18,17 @@ fn read<R: Read>(io: R) -> Result<Vec<u32>, Error> {
         .collect()
 }
 
-fn fuel_required(pkg_mass: &u32) -> u32 {
-    let fuel_req = (pkg_mass / 3) - 2;
-    fuel_req
+fn fuel_required(fuel_mass: &u32) -> u32 {
+    // Here we check on 6 because it will result in 0 fuel needed.
+    // Also Rust shits a brick over attempting to subtract with overflow otherwise. Which is good.
+    if fuel_mass < &6 {
+        return 0
+    }
+    let mut mass : u32 = (fuel_mass / 3) - 2;
+//    println!("Fuel Mass: {}, Mass for mass: {}", fuel_mass, mass);
+    // Accumulate on returns
+    mass += fuel_required(&mass);
+    mass
 }
 
 fn main() -> Result<(), Error> {
@@ -28,18 +36,20 @@ fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     let input = &args[2];
 
-//    let path = env::current_dir().unwrap();
-//    println!("PWD: {}", path.display());
-//    println!("Input: {}", input);
-
     println!("Reading contents from {}", input);
     let packages = read(fs::File::open(input)?)?;
 
-    let mut req_weight : u32 = 0;
-    for package in &packages {
-        req_weight += fuel_required(package);
-    }
+    // The file read function inspired me to try a super simple closure
+    // This was the old, non-recursive fuel calculation function.
+    // Once it went recursive a closure was no longer the right way (I believe anyway)
+    let f_req = |pkg_mass: &u32| ((pkg_mass / 3) - 2) as u32;
 
-    println!("Required fuel mass: {}", req_weight);
+    // accumulate results
+    let mut package_fuel_req : u32 = 0;
+    for package in &packages {
+        package_fuel_req += fuel_required(package);
+    }
+    println!("Required fuel for packages and extra fuel: {}", package_fuel_req);
+
     Ok(())
 }
