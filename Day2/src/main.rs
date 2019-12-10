7b0e922c18f60990656e635060996cd73e1cb752
DTPrince <aaoeaaoe08>
2019-12-10 15:35:29 -0700

    Just some scatter-brained stuff

--- Day2/src/main.rs
+++ Day2/src/main.rs
@@ -6,6 +6,18 @@
 use std::error::Error;
 use std::io;
 
+// R must implement Read
+fn read_fcsv<R: Read>(io : R) -> Result<mut Vec<u32>, Error> {
+    let mut csv_rdr = csv::Reader::from_reader(io);
+    let mut csv_vec = Vec<u32>;
+    for result in csv_rdr.records() {
+        let item = result?;
+
+    }
+    Ok(csv_vec)
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
